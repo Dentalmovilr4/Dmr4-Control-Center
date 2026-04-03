04f6d914d08783e20d65a58cc60187a6ca849566
Dentalmovilr4 <115885210+Dentalmovilr4@users.noreply.github.com>
2026-04-03 15:45:58 -0500

    Crear storage.py

---
+++ storage.py
@@ -0,0 +1,23 @@
+import json
+from datetime import datetime
+import os
+
+FILE = "data/history.json"
+
+def load_history():
+    if not os.path.exists(FILE):
+        return []
+
+    with open(FILE, "r") as f:
+        return json.load(f)
+
+def save_snapshot(coins):
+    history = load_history()
+
+    history.append({
+        "timestamp": datetime.now().isoformat(),
+        "coins": coins
+    })
+
+    with open(FILE, "w") as f:
+        json.dump(history, f, indent=2)
