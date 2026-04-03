62f15bda0975536b07dc19203d3ecffe4cbb1359
Dentalmovilr4 <115885210+Dentalmovilr4@users.noreply.github.com>
2026-04-03 17:11:48 -0500

    Actualizar storage.py

--- storage.py
+++ storage.py
@@ -4,13 +4,23 @@ from datetime import datetime
 
 FILE = "data/history.json"
 
+def ensure_directory():
+    folder = os.path.dirname(FILE)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
 def load_history():
+    ensure_directory()
+
     if not os.path.exists(FILE):
         return []
+
     with open(FILE, "r") as f:
         return json.load(f)
 
 def save_snapshot(coins):
+    ensure_directory()
+
     history = load_history()
 
     history.append({
@@ -20,13 +30,3 @@ def save_snapshot(coins):
 
     with open(FILE, "w") as f:
         json.dump(history, f, indent=2)
-
-def get_coin_history(history, name):
-    results = []
-
-    for snapshot in history:
-        for coin in snapshot["coins"]:
-            if coin["name"] == name:
-                results.append(coin)
-
-    return results[-10:]  # últimas 10 apariciones
